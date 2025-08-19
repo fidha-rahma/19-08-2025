ea0043a76a381a44bca69c63a350633a32be41c4
fidha-rahma <fidhadr17@gmail.com>
2025-08-19 11:03:18 +0700

    Add files via upload

---
+++ AccountTest.java
@@ -0,0 +1,26 @@
+public class AccountTest {
+    public static void main(String[] args) {
+        SavingAccount account1 = new SavingAccount();
+        account1.name = "123456789";
+        account1.balance = 1000.0;
+        account1.interestRate = 0.05;
+        account1.displayCustomer();
+
+        SavingAccount account2 = new SavingAccount();
+        account2.name = "987654321";
+        account2.balance = 9000.;
+        account2.interestRate = 0.03;
+        account2.displayCustomer();
+
+        SavingAccount account3 = new SavingAccount();
+        account3.name = "456789123";
+        account3.balance = 5000.0;
+        account3.interestRate = 0.04;
+        account3.displayCustomer();
+
+        
+
+
+    }
+    
+}
---
+++ SavingAccount.java
@@ -0,0 +1,15 @@
+public class SavingAccount {
+    public double balance;
+    public double interestRate;
+    public String name;
+
+    public void displayCustomer() {
+        System.out.println("Costumer: " + name);
+        System.out.println("Balance: " + balance);
+        System.out.println("Rate: " + interestRate);
+    }
+
+
+    
+}
+
